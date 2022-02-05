c653b1c202d1b37d5980764611ba4d25db8788bf
AntoniuszXQ <76694780+AntoniuszXQ@users.noreply.github.com>
2022-02-05 16:52:27 +0100

    urls updated

--- recruitment_task/core/urls.py
+++ recruitment_task/core/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("investors/", views.InvestorsView.as_view(), name="investors"),
     path("investors/<int:pk>/", views.InvestorDetailsView.as_view(), name="investor-details"),
     path("investors/<int:pk>/invest/<int:project_id>/", views.InvestIntoProject.as_view(), name="invest-into-project"),
+    path("projects/<int:pk>/matches/", views.MatchingInvestorForProject.as_view(), name="matching-investors-for-project"),
+    path("investors/<int:pk>/matches/", views.MatchingProjectsForInvestor.as_view(), name="matching-projects-for-investor")
 ]
